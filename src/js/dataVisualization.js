3202ab43e51084be3cf587c97342a3e01d3e631a
Vladimir Sitchikhin <volodya95rus@icloud.com>
2023-03-08 18:04:15 +0300

    Фикс бага несуществующего элемента

--- src/js/dataVisualization.js
+++ src/js/dataVisualization.js
@@ -52,7 +52,7 @@ class DataVisualization {
 
   getValue(value) {
     let hex, color = "#"
-  
+
     for (var i = 0; i < 3; i++) {
       hex = Math.round(
         this._fromColor[i] + (this._toColor[i] - this._fromColor[i]) * ((value - this.min) / (this.max - this.min))
@@ -66,7 +66,7 @@ class DataVisualization {
 
   hexToRgb(h) {
     let r = 0, g = 0, b = 0
-  
+
     if (h.length == 4) {
       r = "0x" + h[1] + h[1]
       g = "0x" + h[2] + h[2]
